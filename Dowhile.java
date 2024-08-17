89b018a0767868b85941be23b0d177d2dd97652b
teja-saladi <151909654+teja-saladi@users.noreply.github.com>
2024-08-17 12:39:58 +0530

    Create Dowhile.java

---
+++ Dowhile.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Dowhiledemo
+{
+	public static void main(String args[])
+	{
+		int i=0;
+		do
+		{
+		   System.out.println("Helloworld");
+		   i++;
+		}while(i<3);
+	}
+}
