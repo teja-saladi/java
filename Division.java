05bfafeed438db47556dd5539e9c2f77cfb117fe
teja-saladi <151909654+teja-saladi@users.noreply.github.com>
2024-08-17 12:47:35 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class ModDivision
+{
+	public static void main(String[] args)
+	{
+		int a =5;
+		int b= 10;
+		int c = a%b;
+		System.out.println("Division of "+a+"and"+b+"is"+c);
+	}
+}
