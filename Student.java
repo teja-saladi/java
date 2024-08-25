87cef60207eb85753d9044687c441a5072ac2527
teja-saladi <151909654+teja-saladi@users.noreply.github.com>
2024-08-25 17:18:16 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Student
+{
+    
+    String Rollno;
+    float Avg;
+    public static void main(String args[])
+    {
+        Rollno="23k61A05G1";
+        Average=9.0;
+        System.out.println("Rollno is "+ "Rollno");
+        System.out.println("Average is "+ "Average");
+    }
+}
