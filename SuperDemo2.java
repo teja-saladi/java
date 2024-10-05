989d701c9b33808dbf1a3a2474b5ee3cb2fbf782
teja-saladi <151909654+teja-saladi@users.noreply.github.com>
2024-10-05 10:32:43 +0530

    Create SuperDemo2.java

---
+++ SuperDemo2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Animal {
+    String color = "white"; 
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends Animal {
+    String color = "black"; 
+    void printcolor() {
+        System.out.println("Dog color is " + color); 
+        System.out.println("Animal color is " + super.color); 
+    }
+    void eat() {
+        System.out.println("Dog is eating");
+    }
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+    void work() {
+        printcolor(); 
+        bark(); 
+        eat();
+    }
+}
+class SuperDemo2 {
+    public static void main(String args[]) {
+        Dog d = new Dog(); 
+        d.work(); 
+    }
+}
