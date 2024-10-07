dabff68169736e923377f6a9b3114f1ee470e4cb
teja-saladi <151909654+teja-saladi@users.noreply.github.com>
2024-10-07 21:16:14 +0530

    Create Trycatch2.java

---
+++ Trycatch2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Trycatch2{
+    public static void main(String args[])
+    {
+        int a=5,b=0,c;
+        c=a/b;
+        System.out.println("value of c is:"+c);
+    }
